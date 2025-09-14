708203f66a374a487cb1c0299960882540b4efbf
Suthish V <suthishv092@gmail.com>
2025-09-14 13:49:07 +0530

    Time: 0 ms (100%), Space: 42.6 MB (32.09%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class Solution {
+    public double myPow(double x, int n) {
+       double res= Math.pow(x,n);
+    return res;
+    }
+    // public static void main(String[] args){
+    //     Scanner sc=new Scanner(System.in);
+    //     Solution ob=new Solution();
+    //     double x=sc.nextDouble();
+    //     int n=sc.nextInt();
+    //     System.out.print(ob.myPow(x,n));
+    // }
+}
