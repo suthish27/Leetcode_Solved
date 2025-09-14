ebe4dadbce98ada42e1d2fb6a939f4c4ce2ada30
Suthish V <suthishv092@gmail.com>
2025-09-14 13:49:56 +0530

    Time: 2093 ms (33.49%), Space: 40.6 MB (43.38%) - LeetHub

---
+++ 0507-perfect-number/0507-perfect-number.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean checkPerfectNumber(int num) {
+       int count=0;
+        for(int i=1;i<num;i++){
+            if(num%i==0){
+                count+=i;
+            }
+        }
+        if(count==num){
+            return true;
+        }else{
+            return false;
+        }
+    }
+}
